364146058d0842da6c15f5a79b9424e5c20bee21
Sachin Shelke <48155218+hisachinshelke@users.noreply.github.com>
2023-05-30 15:37:50 +0530

    Created New Project
    Dependencies used are Spring Web, JPA, Devtools and MySQL driver

---
+++ src/main/java/com/shelke/sachin/springbootcrudproject/SpringBootCrudProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shelke.sachin.springbootcrudproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shelke/sachin/springbootcrudproject/SpringBootCrudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shelke.sachin.springbootcrudproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
